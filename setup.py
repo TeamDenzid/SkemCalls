c7ae0cdc5c28812d03645cf409b6c7bbdd3c299c
TeamDenzid <84074951+TeamDenzid@users.noreply.github.com>
2021-05-12 10:39:35 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,105 @@
+import os
+import sys
+
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class PostInstall(install):
+    @staticmethod
+    def get_version(package_check):
+        result_cmd = os.popen(f'{package_check} -v').read()
+        result_cmd = result_cmd.replace('v', '')
+        if len(result_cmd) == 0:
+            return {
+                'version_int': 0,
+                'version': '0',
+            }
+        return {
+            'version_int': int(result_cmd.split('.')[0]),
+            'version': result_cmd,
+        }
+
+    # noinspection PyBroadException
+    def run(self):
+        node_result = self.get_version('node')
+        npm_result = self.get_version('npm')
+        if node_result['version_int'] == 0:
+            raise Exception('Please install node (15.+)')
+        if npm_result['version_int'] == 0:
+            raise Exception('Please install npm (7.+)')
+        if node_result['version_int'] < 15:
+            raise Exception(
+                'Needed node 15.+, '
+                'actually installed is '
+                f"{node_result['version']}",
+            )
+        if npm_result['version_int'] < 7:
+            raise Exception(
+                'Needed npm 7.+, '
+                'actually installed is '
+                f"{npm_result['version']}",
+            )
+        os.system('npm install')
+        folder_package = ''
+        for item in sys.path:
+            if 'dist-packages' in item or 'site-packages' in item:
+                folder_package = item
+                break
+        if 'pip' in os.getcwd():
+            print(
+                'Copying files from '
+                f'{os.getcwd()}/SkemCalls/'
+                ' to '
+                f'{folder_package}/SkemCalls/',
+            )
+            if not os.path.exists(
+                f'{folder_package}/SkemCalls',
+            ):
+                os.system(
+                    'mkdir '
+                    f'{folder_package}/SkemCalls',
+                )
+            if os.path.exists(
+                    f'{folder_package}/SkemCalls/dist',
+            ):
+                os.system(
+                    'rm -r '
+                    f'{folder_package}'
+                    '/SkemCalls/dist',
+                )
+            if os.path.exists(
+                    f'{folder_package}'
+                    '/SkemCalls/node_modules',
+            ):
+                os.system(
+                    'rm -r '
+                    f'{folder_package}'
+                    '/SkemCalls/node_modules',
+                )
+            os.system(
+                'cp -r node_modules/ '
+                f'{folder_package}/SkemCalls/node_modules',
+            )
+            os.system(
+                'cp -r SkemCalls/dist/ '
+                f'{folder_package}/SkemCalls/dist',
+            )
+        elif 'bdist_wheel' in sys.argv[2]:
+            if os.path.exists(
+                'SkemCalls/node_modules/',
+            ):
+                os.system(
+                    'rm -r SkemCalls/node_modules/',
+                )
+            os.system(
+                'cp -r node_modules/ SkemCalls/node_modules/',
+            )
+        install.run(self)
+
+
+setup(
+    cmdclass={
+        'install': PostInstall,
+    },
+)
